471437dcf1187ce882597a5c96653b3f12e24d57
Hüseyin Gürel <85082076+huseyingurel1@users.noreply.github.com>
2022-10-08 19:40:35 +0300

    Homework11
    Homework11 added

--- patikaÖdev/SQL-Homeworks.sql
+++ patikaÖdev/SQL-Homeworks.sql
@@ -249,6 +249,34 @@
 
 
 
+# HOMEWORK11
+
+
+## Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+-- actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+      
+      (SELECT first_name FROM customer ORDER BY first_name)
+      UNION
+      (SELECT first_name FROM actor ORDER BY first_name)
+
+-- actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+
+      (SELECT first_name FROM customer ORDER BY first_name)
+      INTERSECT
+      (SELECT first_name FROM actor ORDER BY first_name)
+      
+-- actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+
+      (SELECT first_name FROM customer ORDER BY first_name)
+      EXCEPT
+      (SELECT first_name FROM actor ORDER BY first_name)
+      
+-- İlk 3 sorguyu tekrar eden veriler için de yapalım.
+
+      (SELECT first_name FROM customer ORDER BY first_name)
+      UNION ALL
+      (SELECT first_name FROM actor ORDER BY first_name)
 
 
 
