b296e0940c8201ceb0d3c1f20ddccade0e9b7237
Hüseyin Gürel <85082076+huseyingurel1@users.noreply.github.com>
2022-10-08 17:04:17 +0300

    homework9 updated

--- patikaÖdev/SQL-Homeworks.sql
+++ patikaÖdev/SQL-Homeworks.sql
@@ -225,7 +225,7 @@
 
 -- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
        
-       SELECT rental_id, first_name, last_name FROM rental INNER JOIN customer ON rental.rental_id= customer.customer_id;
+       SELECT rental_id, first_name, last_name FROM rental INNER JOIN customer ON rental.customer_id = customer.customer_id;
 
 
 
