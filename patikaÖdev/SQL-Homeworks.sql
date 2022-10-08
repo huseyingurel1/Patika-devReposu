8e4c577cd54a4dcd0ec1ffa57fe15539ab445040
Hüseyin Gürel <85082076+huseyingurel1@users.noreply.github.com>
2022-10-08 14:33:42 +0300

    Homework6

--- patikaÖdev/SQL-Homeworks.sql
+++ patikaÖdev/SQL-Homeworks.sql
@@ -111,3 +111,29 @@
 -- customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
        
        SELECT * FROM customer WHERE store_id = 1  ORDER BY last_name DESC LIMIT 4; 
+
+
+# HOMEWORK6
+
+
+## Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+-- film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+       
+       SELECT AVG(rental_rate) FROM film;
+
+-- film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+
+       SELECT COUNT(*) FROM film WHERE title LIKE 'C%';
+       
+-- film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+
+       SELECT length FROM film WHERE rental_rate = 0.99 ORDER BY length DESC LIMIT 1;
+       
+-- film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+
+       SELECT COUNT (DISTINCT replacement_cost) FROM  film WHERE length >150;
+
+
+# HOMEWORK7
+
