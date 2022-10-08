21072a77ecbed8db3e7fd65882f84f44a8d7a32c
Hüseyin Gürel <85082076+huseyingurel1@users.noreply.github.com>
2022-10-08 20:43:25 +0300

    Homework12
    Homework12 added

--- patikaÖdev/SQL-Homeworks.sql
+++ patikaÖdev/SQL-Homeworks.sql
@@ -280,3 +280,28 @@
 
 
 
+# HOMEWORK12
+
+## Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+-- film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+
+       SELECT COUNT(length) FROM film WHERE length > (SELECT AVG(length) FROM film) ;
+
+-- film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+
+       SELECT COUNT(rental_rate) FROM film WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+-- film tablosunda en düşük rental_rate ve en düşük replacement_cost değerlerine sahip filmleri sıralayınız.
+
+       SELECT * FROM film WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+-- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+       SELECT * FROM customer FULL JOIN payment ON payment.customer_id = customer.customer_id WHERE amount = (SELECT MAX(amount) FROM payment) ORDER BY customer.customer_id;  
+
+
+
+
+
+
