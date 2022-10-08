a6986956043f73694352f9575d9829682610e325
Hüseyin Gürel <85082076+huseyingurel1@users.noreply.github.com>
2022-10-08 11:31:31 +0300

    HOMEWORK2
    homewor2 added

--- patikaÖdev/SQL-Homeworks.sql
+++ patikaÖdev/SQL-Homeworks.sql
@@ -5,17 +5,40 @@
 
 - film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 
-      ' SELECT title, description FROM film ; '
+       SELECT title, description FROM film ; 
 
 - film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
-      ' SELECT * FROM film WHERE length BETWEEN 60 AND 75;  '
 
+      SELECT * FROM film WHERE length BETWEEN 60 AND 75;  
+      
 - film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
-      ' SELECT * FROM film WHERE rental_rate = 0.99 AND replacement_cost IN (12.99 , 28.99); '
+
+      SELECT * FROM film WHERE rental_rate = 0.99 AND replacement_cost IN (12.99 , 28.99); 
       
 - customer tablosunda bulunan first_name sütunundaki değeri 'Mary' olan müşterinin last_name sütunundaki değeri nedir?
-      ' SELECT last_name FROM customer WHERE first_name = 'Mary' ;   '
+
+      SELECT last_name FROM customer WHERE first_name = 'Mary' ;  
 
 - film tablosundaki uzunluğu(length) 50 ten büyük OLMAYIP aynı zamanda rental_rate değeri 2.99 veya 4.99 OLMAYAN verileri sıralayınız.
 
-      ' SELECT * FROM film WHERE NOT (length > 50) AND NOT(rental_rate IN(2.99, 4.99)); '
+      SELECT * FROM film WHERE NOT (length > 50) AND NOT(rental_rate IN(2.99, 4.99));
+
+
+
+
+# HOMEWORK2
+
+
+## Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+- film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma koşuluyla sıralayınız ( BETWEEN - AND yapısını kullanınız.)
+ 
+      SELECT * FROM film WHERE replacement_cost BETWEEN 12.99 AND 16.98 ; 
+ 
+- .actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' değerleri olması koşuluyla sıralayınız. ( IN operatörünü kullanınız.)
+      
+      SELECT first_name, last_name FROM actor WHERE first_name IN ('Penelope','Nick', 'Ed') ;      
+
+- film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla sıralayınız. ( IN operatörünü kullanınız.)
+
+      SELECT * FROM film WHERE rental_rate IN (0.99, 2.99, 4.99) AND replacement_cost IN ( 12.99, 15.99, 28.99 ) ;
